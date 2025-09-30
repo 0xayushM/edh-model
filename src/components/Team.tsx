e3d7f8cf0541bd9fd19090bd95355dac7519d6c0
Ayush Mangal <mangal.ayush.4982@gmail.com>
2025-10-01 00:21:27 +0530

    team section added

---
+++ src/components/Team.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import Footer from './Footer'
+const Team = () => {
+    return (
+        <>
+        <section className='w-screen h-screen flex p-8'>
+            <div className='w-full px-5 lg:px-24 flex flex-col items-end justify-center'>
+                <h1 className='text-tertiary text-3xl md:text-4xl nebulax uppercase w-full tracking-tight'>The EDHWay</h1>
+            </div>
+        </section>
+        <Footer/>
+        </>
+    )
+}
+
+export default Team
