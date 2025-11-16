5c4b806a9b6bcb87777ac522bdf539e212b74c21
Ayush Mangal <mangal.ayush.4982@gmail.com>
2025-11-16 20:56:54 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,6 @@ const TriangleIcon = () => (
 
 const Footer = () => {
   const socialLinks = Object.entries(company.contact.social);
-  const midIndex = Math.ceil(socialLinks.length / 2);
-  const firstColumn = socialLinks.slice(0, midIndex);
-  const secondColumn = socialLinks.slice(midIndex);
 
   return (
     <footer className="relative w-full text-foreground pt-10 pb-5 md:px-10 font-sans " >
@@ -21,20 +18,7 @@ const Footer = () => {
           <div>
             <h3 className="text-sm uppercase archimoto-bold tracking-[0.4em] mb-8 text-tertiary">Connect with us</h3>
             <ul className="space-y-4">
-              {firstColumn.map(([platform, url]) => (
-                <li key={platform}>
-                  <a href={url} target="_blank" rel="noopener noreferrer" className="flex items-center text-xl md:text-2xl archimoto-bold transition-colors duration-300">
-                    <TriangleIcon />
-                    <span className="capitalize tracking-tighter button-wipe-hover" data-text={platform}>{platform}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="sm:pt-[52px]"> 
-            <ul className="space-y-4">
-              {secondColumn.map(([platform, url]) => (
+              {socialLinks.map(([platform, url]) => (
                 <li key={platform}>
                   <a href={url} target="_blank" rel="noopener noreferrer" className="flex items-center text-xl md:text-2xl archimoto-bold transition-colors duration-300">
                     <TriangleIcon />
@@ -64,7 +48,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="md:text-right pt-0 md:mt-[30px] grid grid-cols-2">
+        <div className="md:text-right pt-0 grid grid-cols-1">
           <div className="mb-6">
             <h4 className="text-sm uppercase archimoto-bold tracking-[0.2em] text-gray-500 mb-2">Email</h4>
             <a href={`mailto:${company.contact.email}`} className="text-md transition-colors archimoto-bold button-wipe-hover duration-300" data-text={company.contact.email}>{company.contact.email}</a>
@@ -77,7 +61,7 @@ const Footer = () => {
 
       </div>
        <div className="text-center text-gray-600 mt-5 text-sm archimoto-bold">
-        © {new Date().getFullYear()} {company.name}. All Rights Reserved.
+        © {new Date().getFullYear()} {company.name}. All Rights Reserved. 
       </div>
     </footer>
   );
